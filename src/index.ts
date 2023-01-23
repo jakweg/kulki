d892f1e8e2beedf2c7f93067737340657b17e4ef
Jakub Wegrzyn <jakubek.weg@gmail.com>
2023-01-23 11:39:18 +0100

    Hide install prompt

--- src/index.ts
+++ src/index.ts
@@ -90,3 +90,5 @@ document.addEventListener('visibilitychange', () => {
 window.addEventListener('blur', () => performSerialization())
 initLanguage()
 document.body.classList.remove('loading')
+
+window.addEventListener("beforeinstallprompt", e => e.preventDefault());
