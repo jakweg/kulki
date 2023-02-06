d0a6ca48f03d2d2e59288a3a99628893de91a5a7
Jakub Wegrzyn <jakubek.weg@gmail.com>
2023-02-06 08:38:53 +0100

    Make polish language a default one

--- src/lang.ts
+++ src/lang.ts
@@ -1,27 +1,27 @@
 const lang = navigator.language.toLocaleLowerCase()
 const isPolish = lang === 'pl' || lang === 'pl-pl'
 
-const setUpManifest = (variant: string | null) => {
+const setUpManifest = (variant: string) => {
     const link = document.createElement('link')
     link.setAttribute('rel', 'manifest')
-    link.setAttribute('href', `manifest${variant ? ('-' + variant) : ''}.json`)
+    link.setAttribute('href', `manifest-${variant || 'en'}.json`)
     document.head.appendChild(link)
 }
 
-const setPl = () => {
-    document.documentElement.setAttribute('lang', 'pl')
-    document.title = 'Kulki'
-    document.querySelector('meta[name="description"]').setAttribute('content', "Odpocznij sobie i zagraj w Kulki")
-    document.getElementById('title').textContent = 'Kulki'
-    document.getElementById('score-prefix').textContent = 'Twój wynik to '
-    document.getElementById('next-prefix').textContent = 'Kolejne kolory:'
+const setEnglishLanguage = () => {
+    document.documentElement.setAttribute('lang', 'en')
+    document.title = 'Color lines'
+    document.querySelector('meta[name="description"]').setAttribute('content', "Relax and play Color lines!")
+    document.getElementById('title').textContent = 'Color lines'
+    document.getElementById('score-prefix').textContent = 'Your score is '
+    document.getElementById('next-prefix').textContent = 'Next colors:'
 
-    setUpManifest('pl')
+    setUpManifest('en')
 }
 
 export const init = () => {
-    if (isPolish) setPl()
-    else setUpManifest(null)
+    if (isPolish) setUpManifest('pl')
+    else setEnglishLanguage()
 }
 
 export const getLostText = (score: number) => {
